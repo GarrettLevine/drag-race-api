ae1404837837c6759e796883d8f3cace5bd2d2a2
kevanhannah <kevan.hannah@gmail.com>
2017-04-18 21:46:34 -0400

    creating get/create for queens

---
+++ router/index.js
@@ -0,0 +1,9 @@
+const express = require(`express`);
+
+const router = express.Router();
+
+const queenRouter = require(`./queens`);
+
+router.use(`/queens`, queenRouter);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require(`express`);
+const mongoose = require(`mongoose`);
+const bodyParser = require(`body-parser`);
+
+const {
+    username,
+    password,
+    url,
+    dbPort
+} = require(`./private-config`);
+const router = require(`./router/index.js`)
+
+mongoose.connect(`mongodb://${username}:${password}@${url}:${dbPort}/drag-race-api`);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 8080;
+
+app.use(`/api`, router);
+
+app.listen(port)
+console.log(`App is running on port ${port}`);
