8af9a8be9047d46fdbc34d1c2cc82c445f5af97f
kevanhannah <kevan.hannah@gmail.com>
2017-04-24 20:09:59 -0400

    updated branch

--- models/Queen.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require(`mongoose`);
-const Schema = mongoose.Schema;
-
-const QueenSchema = new Schema({
-  name: String,
-});
-
-module.exports = mongoose.model(`Queen`, QueenSchema);
