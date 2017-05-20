c591cd7fd00751ff2f5497150496823a1c346636
Garrett Levine <garrlevine@gmail.com>
2017-05-20 08:25:38 -0400

    season endpoint

--- endpoints/seasons/get-seasons.js
+++ endpoints/seasons/get-seasons.js
@@ -5,7 +5,9 @@ const {
 
 function getSeasons(req, res) {
   return Season.findAll({
-    include: [Queen],
+    include: [
+      { model: Queen, attributes: [ 'name', 'id' ] },
+    ],
   })
   .then(seasons => res.json(seasons))
   .catch(err => res.json(err));
