0cb59e2d07bb901849c20a113747565406101b87
Garrett Levine <garrlevine@gmail.com>
2017-06-26 19:08:08 -0400

    added CORS to server

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const port = process.env.PORT || 8080;
 
