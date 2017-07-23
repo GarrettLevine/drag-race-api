0693e6e45d92d3c54738c6a528eadf44ff1543af
Garrett Levine <garrlevine@gmail.com>
2017-07-23 14:16:20 -0400

    added try/catch

--- models/db.js
+++ models/db.js
@@ -5,16 +5,21 @@ const dbName = process.env.DB_NAME;
 const dbUsername = process.env.DB_USERNAME;
 const dbPassword = process.env .DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
-const dbPort = process.env.PORT;
-console.log(dbName, dbUsername, dbPort, dbHost, dbPort);
+const dbPort = process.env.DB_PORT;
 
-const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
-  host: dbHost,
-  port: dbPort,
-  dialect: 'postgres',
-  logging: false,
-  pool: { maxConnections: 100, maxIdleTime: 30 },
-  retry: { max: 3 },
-});
+const sequelize = () => {
+  try {
+    return new Sequelize(dbName, dbUsername, dbPassword, {
+      host: dbHost,
+      port: dbPort,
+      dialect: 'postgres',
+      logging: false,
+      pool: { maxConnections: 100, maxIdleTime: 30 },
+      retry: { max: 3 },
+    });
+  } catch(ex)  {
+    console.log(ex);
+  }
+};
 
 module.exports = sequelize;
