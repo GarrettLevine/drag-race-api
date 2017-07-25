0607000bd690c7d405f1dc258db1efa92903c7da
Garrett Levine <garrlevine@gmail.com>
2017-07-25 09:03:21 -0400

    updates to DB

--- models/db.js
+++ models/db.js
@@ -7,6 +7,7 @@ const dbPassword = process.env .DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 const dbPort = process.env.DB_PORT;
 
+console.log(dbName, dbHost, dbPassword)
 const sequelize = new Sequelize(dbName, dbUsername, dbPassword, {
   host: dbHost,
   port: dbPort,
