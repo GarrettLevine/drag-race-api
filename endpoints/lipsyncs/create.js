a7094cba414f5bb034de164ed4adbc299895aeaf
Garrett Levine <garrlevine@gmail.com>
2017-08-02 18:44:46 -0400

    updated lipsync create

--- endpoints/lipsyncs/create.js
+++ endpoints/lipsyncs/create.js
@@ -4,17 +4,11 @@ const {
 } = require('models');
 
 function create(req, res) {
-  let lipsync;
   return Lipsync.create({
     name: req.body.name,
     artist: req.body.artist,
     episodeId: req.body.episodeId,
   })
-  .then((l) => {
-    lipsync = l;
-    return Episode.findById(req.body.episodeId);
-  })
-  .then((episode) => lipsync.addEpisode(episode))
   .then(lipsync => res.json(lipsync))
   .catch(err => res.json(err));
 }
