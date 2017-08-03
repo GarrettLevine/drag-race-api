cfa31fdd8b87fae1e09cd9516d76d5cd6eee5c28
Garrett Levine <garrlevine@gmail.com>
2017-08-03 08:46:42 -0400

    cha-cha-changes

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const router = require(`./router/index.js`)
-const { rateLimit } = require('./middleware/')
+const { rateLimit } = require('./middleware')
 
 'use strict';
 
-
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.enable('trust proxy');
 
@@ -19,8 +19,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
-const port = process.env.PORT || 8080;
 
 app.use(`/api`, router);
 app.get('/', (req, res) => {
