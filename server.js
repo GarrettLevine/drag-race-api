44b53a5d19bb2aa770bc06feecea6546fa44bf04
Garrett Levine <garrlevine@gmail.com>
2017-09-12 14:54:21 +0900

    removed allow methods

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, HEAD');
   res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
