8eb74a9993622b5c56bb18ff7cfb469c5f3f848d
Garrett Levine <garrlevine@gmail.com>
2017-10-17 20:20:37 -0400

    get-challenges-by-episodes

---
+++ endpoints/challenges/get-season-challenges.js
@@ -0,0 +1,41 @@
+const {
+    Challenge,
+    Season,
+    Queen,
+} = require('models');
+
+const {
+  formatChallenge,
+  errorHandler: eh,
+} = require('utils');
+
+function getSeasnsChallenges(req, res) {
+    return Season.findById(req.params.id)
+        .then(season => {
+            if (!season) eh.noSeasonWithId(req.params.id);
+
+            return season.getEpisodes();
+        })
+        .then(episodes => {
+            return Challenge.findAll({
+                where: {
+                    episodeId: episodes.map(e => e.id),
+                },
+                include: [{
+                    model: Queen,
+                    through: {
+                        attributes: ['won'],
+                    },
+                }],
+            });
+        })
+        .then(challenges => {
+            if (!challenges.length) return Promise.reject(eh.noQueenWithId(req.params.id));
+            
+            const formattedChallenge = challenges.map(c => formatChallenge(c));
+            res.status(200).json(formattedChallenge);
+        })
+        .catch(err => res.json(eh.handleError(err)));
+}
+
+module.exports = getSeasnsChallenges;
--- endpoints/challenges/index.js
+++ endpoints/challenges/index.js
@@ -4,6 +4,7 @@ c.getChallenges = require('./get-challenges');
 c.getChallengeById = require('./get-challange-by-id')
 c.getQueenChallenges = require('./get-queens-challenges');
 c.getEpisodeChallenges = require('./get-episode-challenges');
+c.getSeasonChallenges = require('./get-season-challenges');
 c.create = require('./create');
 
 module.exports = c;
