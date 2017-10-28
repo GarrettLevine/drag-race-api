771d08657307ac38b23a9421f8646f258a5875ba
Garrett Levine <garrlevine@gmail.com>
2017-10-28 10:31:27 -0400

    adds al queens endpoint for label/ids

--- endpoints/queens/get-all-queens-names.js
+++ endpoints/queens/get-all-queens-names.js
@@ -4,11 +4,11 @@ const { errorHandler: eh } = require('utils');
 function getAllQueenNames(req, res) {
     return Queen.findAll()
         .then(queens => {
-            const queenArray = queens.map(q => ({
+            const queensArray = queens.map(q => ({
                 name: q.name,
                 id: q.id,
             }));
-            res.status(200).json(queenArray)
+            res.status(200).json(queensArray)
         })
         .catch(err => eh.handleError(err))
 }
