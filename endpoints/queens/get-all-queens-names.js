7b92bd8c1e313adc6bee18721e3dcadc2c382af2
Garrett Levine <garrlevine@gmail.com>
2017-10-28 10:41:02 -0400

    added ascended order to queens calls

--- endpoints/queens/get-all-queens-names.js
+++ endpoints/queens/get-all-queens-names.js
@@ -1,16 +1,20 @@
 const { Queen } = require('models');
-const { errorHandler: eh } = require('utils');
+const { 
+    formatQueen,
+    errorHandler: eh,
+} = require('utils');
 
 function getAllQueenNames(req, res) {
-    return Queen.findAll()
-        .then(queens => {
-            const queensArray = queens.map(q => ({
-                name: q.name,
-                id: q.id,
-            }));
-            res.status(200).json(queensArray)
-        })
-        .catch(err => eh.handleError(err))
+    return Queen.findAll({
+        order: [
+            ['id', 'ASC']
+        ],
+    })
+    .then(queens => {
+        const queensArray = queens.map(q => formatQueen(q));
+        res.status(200).json(queensArray)
+    })
+    .catch(err => eh.handleError(err))
 }
 
 module.exports = getAllQueenNames;
--- utils/format-queen.js
+++ utils/format-queen.js
@@ -1,19 +1,24 @@
-const formatQueen = queen => {
-  if (typeof queen !== 'object') return {};
+const formatQueen = q => {
+  if (typeof q !== 'object') return {};
 
-  return {
-    id: queen.id,
-    name: queen.name,
-    winner: queen.winner,
-    missCongeniality: queen.missCongeniality,
-    image_url: queen.image_url,
-    quote: queen.quote,
-    seasons: queen.Seasons.map(season => ({
+  const queen = {
+    id: q.id,
+    name: q.name,
+    winner: q.winner,
+    missCongeniality: q.missCongeniality,
+    image_url: q.image_url,
+    quote: q.quote,
+  };
+
+  if (q.Seasons) {
+    queen.seasons = q.Seasons.map(season => ({
       seasonNumber: season.seasonNumber,
       seasonId: season.id,
       place: season.QueensSeasons.place,
-    })),
-  };
+    }));
+  }
+
+  return queen;
 };
 
 module.exports = formatQueen;
