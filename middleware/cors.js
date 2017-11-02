4a68eb90f0ec5de2e270ddcb73b8766fc63bd38a
Garrett Levine <garrlevine@gmail.com>
2017-11-02 08:52:43 -0400

    added method

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,10 +1,6 @@
 module.exports = (req, res, next) => {
-    console.log({ req, res });
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'content-type, application/json'
-    );
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept, application/json, method');
     next();
 }
