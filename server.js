befb79c98dfb927e73c8cdefc87752c59e36bd2a
Garrett Levine <garrlevine@gmail.com>
2017-11-01 22:32:15 -0400

    movin

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.json());
 // Enable CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  // res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(rateLimit);
@@ -28,10 +28,10 @@ app.use(`/api`, router);
 app.get('/', (req, res) => {
   res.status(301).redirect('https://drag-race-api.readme.io/docs');
 })
-app.use(Raven.errorHandler());
 app.get('/*', (req, res) => {
   res.status(400).json({ message: 'no route found.' });
 });
 
+app.use(Raven.errorHandler());
 app.listen(port)
 console.log(`App is running on port ${port}`);
