5d09b3f2ed3e52f4a1f1bf4f7ec791e4126764b0
Garrett Levine <garrlevine@gmail.com>
2017-11-02 08:12:57 -0400

    test

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,9 @@
 module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept, application/json');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'content-type, application/json'
+    );
     next();
 }
