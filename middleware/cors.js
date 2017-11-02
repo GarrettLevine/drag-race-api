d073b20fdd8c1b1090152ff4a3931b87e30c180c
Garrett Levine <garrlevine@gmail.com>
2017-11-02 08:03:42 -0400

    removed logs

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,6 +1,4 @@
 module.exports = (req, res, next) => {
-    console.log({ req });
-    console.log({ res });
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept, application/json');
