8b890cbbe84c95f3a793a7aea3df2ba8e7458803
Garrett Levine <garrlevine@gmail.com>
2017-11-02 08:03:18 -0400

    added application/json

--- middleware/cors.js
+++ middleware/cors.js
@@ -3,6 +3,6 @@ module.exports = (req, res, next) => {
     console.log({ res });
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers', 'content-type, Authorization, Origin, X-Requested-With, Content-Type, Accept, application/json');
     next();
 }
