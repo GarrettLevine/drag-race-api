a4cbca8ff25d3bf419b75d5c562afda99b59bbb3
Garrett Levine <garrlevine@gmail.com>
2018-01-07 21:08:50 -0500

    utils

--- src/Admin/AddEpisode.jsx
+++ src/Admin/AddEpisode.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
 export default class AddEpisode extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      seasonId: 0,
+      activeQueens: [],
+      eliminateQueens: [],
+    };
+  }
+  componentDidMount() {
+    fetch('/api/seasons')
+      .then((resp) => resp.json())
+      .then((seasons) => {
+        const seasonId = seasons.sort((a, b) => b.id - a.id)[0].id;
+        this.setState({
+          seasonId,
+        });
+
+        return fetch(`/api/seasons/${seasonId}/queens`);
+      })
+      .then((resp) => resp.json())
+      .then((queens) => {
+
+      });
+  }
+
   render() {
     return (
       <div> ADD EPISODE </div>
