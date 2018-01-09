6a87c3d2798cc420d510e4cc85355f73fbc766c0
root <root@LAPTOP-95BDLGUR.localdomain>
2018-01-09 16:17:32 -0500

    create files

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { Header } from '../components'
+import LandingHeader from './LandingHeader';
 
 import './main.scss';
 
 export default class Main extends React.Component {
   render() {
     return (
-      <Header header="Main Page" subheader="Get werkin'" />
+      <LandingHeader header="Main Page" subheader="Get werkin'" />
     );
   }
 }
