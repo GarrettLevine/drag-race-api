32b58767b7ad43443f7b0533f99684d2b2386a8c
Mike Walker <mikewalker24@gmail.com>
2018-01-11 14:05:05 -0500

    updated TextInput to use props, not state

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -2,18 +2,10 @@ import React from 'react';
 
 import './TextInput.scss';
 
-export default class TextInput extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      value: '',
-    }
-  }
-  
+export default class TextInput extends React.Component {  
   render() {
     return (
-      <input type="text" value={this.state.value} id={this.props.id} onChange={this.props.handleChange} />
+      <input type="text" value={this.props.value} id={this.props.id} onChange={this.props.handleChange} />
     )
   }
 }
