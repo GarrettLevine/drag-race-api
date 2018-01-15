f97a0f07a22af1e72975a3141e816bfc029cba28
Meaghan Eleanor <meaghan.eleanor@gmail.com>
2018-01-15 13:35:23 -0500

    Reconfigured textarea to use props, updated color variables for textarea border

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -3,25 +3,9 @@ import React from 'react';
 import './Textarea.scss';
 
 export default class Textarea extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.updateDescription = this.updateDescription.bind(this);
-
-    this.state = {
-        challengeDescription: '',
-    };
-  };
-  
-  updateDescription(event) {
-        this.setState({
-            challengeDescription: event.target.value,
-        });
-    }
-
   render() {
       return (
-        <textarea id={this.props.id} placeholder={this.props.placeholder} value={this.state.challengeDescription} onChange={this.updateDescription}></textarea>
+        <textarea id={this.props.id} placeholder={this.props.placeholder} value={this.props.value} onChange={this.props.handleChange}></textarea>
       );
     };
   
