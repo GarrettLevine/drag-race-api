e0227aa21473c125eed45355bfcacd275ed9336b
Benjie Kibblewhite <kibben@gmail.com>
2018-01-16 21:33:28 +0000

    remove unnecesary components from main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -9,42 +9,6 @@ export default class Main extends React.Component {
     return (
       <Wrapper>
             <Header header="Main Page" subheader="Get werkin'" />
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate"
-              />
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate1"
-              />
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate2"
-              />
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate3"
-              />  
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate4"
-              />
-            <Checkbox 
-              label="Kennedy Davenport" 
-              name="eliminatedQueens" 
-              className="checkbox"
-              id="kennedyeliminate5"
-              />  
       </Wrapper>
     );
   }
