9c98ef94a56fd5aaed67b952794178232ac74f69
root <root@LAPTOP-95BDLGUR.localdomain>
2018-01-20 15:03:22 -0500

    build Greeting section

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import './main.scss';
 
-import { Wrapper} from '../components';
+import { Wrapper } from '../components';
 
 import MainHeader from './MainHeader';
+import Greeting from './Greeting';
 
 export default class Main extends React.Component {
   render() {
     return (
       <Wrapper>
             <MainHeader header="Main Page" subheader="Get werkin'" />
+            <Greeting />
       </Wrapper>
     );
   }
