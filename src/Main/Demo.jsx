b62c056b85b432bef01625a9c61b56c2074e28da
root <root@LAPTOP-95BDLGUR.localdomain>
2018-01-28 16:35:12 -0500

    start build

---
+++ src/Main/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Demo.scss';
+
+export default class Demo extends React.Component {
+   render() {
+     return(
+
+     )
+   }
+}
---
+++ src/Main/DemoDropdown.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './DemoDropdown.scss';
+
+export default class DemoDropdown extends React.Component {
+   render() {
+     return(
+
+     )
+   }
+}
