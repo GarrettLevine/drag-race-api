798071f4cb2c8f471bccff379f05f0f6dc965dad
Mike Walker <mikewalker24@gmail.com>
2018-01-28 17:40:37 -0500

    removed lines from main.jsx

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -13,8 +13,6 @@ export default class Main extends React.Component {
   render() {
     return (
       <Wrapper>
-            <Header header="Main Page" subheader="Get werkin'" />
-            <Queens />
             <MainHeader header="Main Page" subheader="Get werkin'" />
             <Greeting />
             <DonateCallToAction />
