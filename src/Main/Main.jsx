859dfe9451bec4561ae2b0cf10efc623550693e8
root <root@LAPTOP-95BDLGUR.localdomain>
2018-01-28 16:11:25 -0500

    start building

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 
 import './main.scss';
 
-import { Wrapper } from '../components';
+import { Wrapper, Checkbox } from '../components';
 
 import MainHeader from './MainHeader';
 import Greeting from './Greeting';
 import DonateCallToAction from './DonateCallToAction';
 import MainFooter from './MainFooter';
-
+import Demo from './Demo';
 export default class Main extends React.Component {
   render() {
     return (
       <Wrapper>
+          <Checkbox
+            id="alaska"
+            className="checkbox_eliminated"
+            name="eliminatedQueens"
+            value="Alaska"
+            label="Alaska Thunderfuck"
+
+          />
             <MainHeader header="Main Page" subheader="Get werkin'" />
             <Greeting />
+            <Demo />
             <DonateCallToAction />
             <MainFooter />
       </Wrapper>
--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -5,19 +5,21 @@ import classNames from 'classnames';
 import './Checkbox.scss';
 
 export default class Checkbox extends React.Component {
-    
+
     render() {
         return (
             //for blue check, give className checkbox_standard
             //for red cross-out, give className checkbox_elimination
             <div className={classNames('checkbox-container',  this.props.className)}>
                <input
+               checked
                     id={this.props.id}
                     className='checkbox'
                     name={this.props.name}
                     type="checkbox"
-                    onChange={this.props.handleChange} 
-                    value={this.props.value}/>
+                    onChange={this.props.handleChange}
+                    value={this.props.value}
+                    />
                 <label className='checkbox-label' htmlFor={this.props.id}>
                     {this.props.label}
                 </label>
