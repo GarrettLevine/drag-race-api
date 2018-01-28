def774691a8e8b77b6fe41daab64c29e65d2be29
root <root@LAPTOP-95BDLGUR.localdomain>
2018-01-28 16:27:22 -0500

    removed checkbox from landing page

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -16,13 +16,6 @@ export default class Main extends React.Component {
             <MainHeader header="Main Page" subheader="Get werkin'" />
             <Greeting />
             <DonateCallToAction />
-            <Checkbox
-
-              id="alaska"
-              name="eliminated_queens"
-              value="alaska"
-              label="Alaska Thunderfuck"
-            />
             <MainFooter />
       </Wrapper>
     );
