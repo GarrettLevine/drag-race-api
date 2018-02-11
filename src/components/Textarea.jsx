fae42582fef076f8c8cc822858396cf189ec1762
Meaghan Eleanor <meaghan.eleanor@gmail.com>
2018-02-10 22:44:04 -0500

    Added labels to textarea, added styling to new textarea labels

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -5,6 +5,7 @@ import './Textarea.scss';
 export default class Textarea extends React.Component {
   render() {
       return (
+      	<label htmlFor={this.props.id} className="textarea-input-label"}{this.props.label}</label>
         <textarea id={this.props.id} placeholder={this.props.placeholder} value={this.props.value} onChange={this.props.handleChange}></textarea>
       );
     };
